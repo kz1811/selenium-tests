a40d803d7e30b467213b1bd221c3f52739a040ec
Your Name <you@example.com>
2021-09-19 12:01:39 +0300

    conftest.py was added

--- conftest.py
+++ conftest.py
@@ -1,27 +1,11 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-supported_browsers = {
-    'chrome': webdriver.Chrome,
-    'firefox': webdriver.Firefox
-}
-
-def pytest_addoption(parser):
-    parser.addoption("--browser_name", action='store', default='chrome', help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-
-    if browser_name in supported_browsers:
-        print(f"\nstart {browser_name} browser for test..")
-        browser = supported_browsers.get("browser_name")()
-    else:
-        joined_browsers = ', '.join(supported_browsers.keys())
-        raise pytest.UsageError(f"--browser_name should be one of: {joined_browsers}")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(executable_path=r'c:\chromedriver\chromedriver.exe', language="en")
     yield browser
     print("\nquit browser..")
     browser.quit()
