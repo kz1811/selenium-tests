afbc1efa7c6a89168de54d401caee679b4e44cae
Your Name <you@example.com>
2021-09-22 01:10:02 +0300

    functions for testing login-page added.

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        CURRENT_URL = webdriver.current_url()
+        assert 'login' in CURRENT_URL, f'expected "login" to be substring of URL'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert True
