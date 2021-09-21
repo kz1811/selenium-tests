0eb98d56e208c82eb2e78a1372f71e1d89ebb18e
Your Name <you@example.com>
2021-09-22 01:24:56 +0300

    login fuction was added for using in test_main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
-
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        log_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        log_link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_irl)
 
     def should_be_login_link(self):
         assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
