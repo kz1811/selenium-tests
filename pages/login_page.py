759ead30559f1d7b252668aaddf748bd05ce87cb
Your Name <you@example.com>
2021-09-23 11:20:23 +0300

    Some bugs were fixed.

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,8 +10,8 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        CURRENT_URL = webdriver.current_url()
-        assert 'login' in CURRENT_URL, f'expected "login" to be substring of URL'
+        CURRENT_URLL = self.browser.current_url
+        assert 'login' in CURRENT_URLL, f'expected "login" to be substring of URL'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
